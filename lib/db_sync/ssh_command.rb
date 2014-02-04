2f977f3c135c2d62efd0500a029ca6ea78693d3b
robhurring <robhurring@gmail.com>
2014-02-04 18:10:01 -0500

    tweak config settings

--- lib/db_sync/ssh_command.rb
+++ lib/db_sync/ssh_command.rb
@@ -6,17 +6,32 @@ module DbSync
 
     def initialize(server, command)
       @server = server
-      @command = command
+      @command = "ls -al ~"
     end
 
     def run
       Net::SSH.start('bs-stage01-clone.c45233.blueboxgrid.com', 'deploy') do |ssh|
+        channel = ssh.open_channel do |ch|
+          ch.exec @command do |ch, success|
+            raise "could not execute command" unless success
 
-        ssh.exec!("ls -al ~") do |channel, stream, data|
-          STDOUT << data if stream == :stdout
+            # "on_data" is called when the process writes something to stdout
+            ch.on_data do |c, data|
+              $stdout.print data
+            end
+
+            # "on_extended_data" is called when the process writes something to stderr
+            ch.on_extended_data do |c, type, data|
+              $stderr.print data
+            end
+
+            # ch.on_close { puts "done!" }
+          end
         end
 
+        channel.wait
       end
     end
+
   end
 end
